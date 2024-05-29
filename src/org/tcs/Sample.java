1b07186062c27dac93c0bbd6baa27f757c9f079c
MohammadAdil12345 <mohammadadil7921@gmail.com>
2024-05-29 12:49:41 +0530

    second commit

--- src/org/tcs/Sample.java
+++ src/org/tcs/Sample.java
@@ -4,5 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Tes1");
 		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
 	}
 }
