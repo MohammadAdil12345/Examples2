401d1d701dc416678a33307848ac58edfdb4ccfc
MohammadAdil12345 <mohammadadil7921@gmail.com>
2024-05-29 12:55:41 +0530

    java code

--- src/org/tcs/Sample.java
+++ src/org/tcs/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Tes1");
 		System.out.println("Test2");
-		System.out.println("Test3");
-		System.out.println("test4");
+	    System.out.println("Test3");
+	    System.out.println("Tset4");
 	}
 }
